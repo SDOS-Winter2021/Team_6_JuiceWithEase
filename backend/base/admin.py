a82897afa5b6b3a6073e33f2aaa3a310de56cd5c
Larika Sehgal <53709131+LarikaSehgal@users.noreply.github.com>
2021-04-08 02:07:17 +0530

    Admin View Customised (Products)

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import * 
 # Register your models here.
 
-admin.site.register(Product)
+admin.site.site_header = 'Juice With Ease - Products & Product Categories' 
 admin.site.register(ProductCategory)
 admin.site.register(Bottletype)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','category_Id']
+    list_filter = ['category_Id']
+
+admin.site.register(Product,ProductAdmin)
