fd75adbe466543f961df031381f064f83877c46c
Sanchit18091 <sanchittrivedi2000@gmail.com>
2021-04-09 18:31:01 +0530

    Prepare Files for Hosting

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponseRedirect
 
-# class CustomSchemeRedirect(HttpResponsePermanentRedirect):
-#     allowed_schemes = ['ftp']
-# def redirect(request):
-#     return CustomSchemeRedirect('ftp:///Users/soumyadeeppaul/Downloads/Team_6_JuiceWithEase/storefront/activate')
-# Create your views here.
+from jwe.settings import STATIC_URL
+
 
 def getActivation(req, uid, token):
-    return HttpResponseRedirect(f'http://localhost:8080/activate?uid={uid}&token={token}')
+    return HttpResponseRedirect(f'http://localhost:8000/{STATIC_URL}activate.html?uid={uid}&token={token}')
--- backend/jwe/settings.py
+++ backend/jwe/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -139,7 +139,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'shop/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'..','storefront'),
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
