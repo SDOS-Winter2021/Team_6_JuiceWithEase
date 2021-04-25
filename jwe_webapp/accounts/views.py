a3a71e72a69680c890a7ec0282b7add2c66efc38
Larika Sehgal <53709131+LarikaSehgal@users.noreply.github.com>
2021-04-25 19:37:54 +0530

    Compiling new changes in hosting format

---
+++ jwe_webapp/accounts/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+
+from jwe.settings import STATIC_URL, SITE_URL
+
+
+def getActivation(req, uid, token):
+    return HttpResponseRedirect(f'{SITE_URL}{STATIC_URL}activate.html?uid={uid}&token={token}')
--- jwe_webapp/jwe/views.py
+++ jwe_webapp/jwe/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponseRedirect
 from .settings import STATIC_URL
 
 def getHome(request):
-    return HttpResponseRedirect(f'{STATIC_URL}index.html')
+    return HttpResponseRedirect(f'http://localhost:8000/{STATIC_URL}index.html')
