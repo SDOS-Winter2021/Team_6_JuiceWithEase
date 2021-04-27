813f243393cf4faa7aeac3949bc1d16a5ab346e0
Larika Sehgal <53709131+LarikaSehgal@users.noreply.github.com>
2021-04-27 22:03:20 +0530

    Added Contact Form (Feedback) Fucntionality

---
+++ jwe_webapp/feedback/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Feedback(models.Model):
+    username = models.CharField(max_length=200, null=True, blank=False)
+    useremail = models.CharField(max_length=200, null=True, blank=False)
+    subject = models.TextField(max_length=200,null=True, blank=False)
+    message = models.TextField(null=True, blank=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
+    _id = models.AutoField(primary_key=True, editable=False, blank=False)
+
+    def __str__(self):
+        return  str(self.subject) + " by " + str(self.username)
---
+++ jwe_webapp/feedback/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from rest_framework.decorators import api_view
+from .models import Feedback
+from .serializers import FeedbackSerializer
+# Create your views here.
+
+@api_view(["POST"])
+def addfeedback(request):
+    # print(request.data['username'])
+    # print(request.data['useremail'])
+    # print(request.data['subject'])
+    # print(request.data['message'])
+    fb = Feedback.objects.create(
+            username=request.data['username'], 
+            useremail=request.data['useremail'], 
+            subject=request.data['subject'],
+            message=request.data['message'],
+    )
+    fb.save()
+    serializer = FeedbackSerializer(fb, many=False)
+    return HttpResponse(serializer)
